955b93be0b99c32c8efd81606e76865936330ad5
Lea TROPEA <leatropea2@gmail.com>
2026-04-21 14:12:46 +0200

    TP et TP2

---
+++ TP2-LOCAL & AWS/Exercice 4 & 5 - Déployer sur AWS et un Bucket/variables.tf
@@ -0,0 +1,23 @@
+variable "instance_type" {
+  description = "Le type d'instance EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Le nom de l'instance EC2"
+  type        = string
+  default     = "nginx-server"
+}
+
+variable "bucket_name" {
+  description = "Le nom du bucket S3"
+  type        = string
+  default     = "lea-tropea"
+}
+
+variable "default_port" {
+  description = "Le port par défaut pour le groupe de sécurité"
+  type        = number
+  default     = 80
+}
